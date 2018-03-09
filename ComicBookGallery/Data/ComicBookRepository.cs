041bb082bf0cdcedf7b83aeb99e16df5f7f98001
Keegan Gladstone <keegangladstone@gmail.com>
2018-03-09 09:48:58 -1000

    Updated default route, added comicBooks List view

--- ComicBookGallery/Data/ComicBookRepository.cs
+++ ComicBookGallery/Data/ComicBookRepository.cs
@@ -55,6 +55,13 @@ namespace ComicBookGallery.Data
             }
         };
 
+        public ComicBook[] GetComicBooks()
+        {
+
+            return _comicBooks;
+        }
+
+
         public ComicBook GetComicBook(int id)
         {
             ComicBook comicBookToReturn = null;
